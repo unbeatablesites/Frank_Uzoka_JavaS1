15c89855db10c31d1f69f2b630bd0cc49de1c874
Frank Uzoka <Frankuzoka@hotmail.com>
2019-08-05 17:04:30 -0400

    Solved Homework

--- 20190806/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
+++ 20190806/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
@@ -3,6 +3,8 @@ package com.company;
 public class FormattedOutput {
 
     public static void main(String[] args) {
+        System.out.format("My name is %s and I am %d years old.", "Frank",35);
+
 
     }
 }
